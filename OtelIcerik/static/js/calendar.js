3340b2b7caf7787dbdbc60d11b8c1fbd6472a8dd
CoderMungan <codermungan@gmail.com>
2023-11-09 04:44:58 +0300

    Blokaj Done && Fixed Some Issues

--- OtelIcerik/static/js/calendar.js
+++ OtelIcerik/static/js/calendar.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
+        locale: 'tr',
         initialView: 'dayGridMonth',
         initialDate: tarih,
         themeSystem: 'standart',
