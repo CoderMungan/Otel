b13e87a8807166a47d03b79c0b551e548189e43f
CoderMungan <codermungan@gmail.com>
2023-11-12 03:17:35 +0300

    fixed some issue

--- OtelIcerik/cronjob.py
+++ OtelIcerik/cronjob.py
@@ -1,14 +1,15 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from .jobs import odaDurumuDegistir
+
+from .jobs import odaDurumuDegistir, odastatus
 
 
 def start_jobs():
     scheduler = BackgroundScheduler()
-    
-    #Set cron to runs every 1 min.
-    cron_job = {'month': '*', 'day': '*', 'hour': '*', 'minute':'*/1'}
-    
-    #Add our task to scheduler.
-    scheduler.add_job(odaDurumuDegistir, 'cron', **cron_job)
-#And finally start.    
+
+    # Set cron to runs every 1 min.
+    cron_job = {"month": "*", "day": "*", "hour": "*", "minute": "*/1"}
+
+    # Add our task to scheduler.
+    scheduler.add_job(odastatus, "cron", **cron_job)
+    # And finally start.
     scheduler.start()
--- OtelIcerik/jobs.py
+++ OtelIcerik/jobs.py
@@ -1,4 +1,5 @@
 from datetime import date, datetime
+from traceback import print_tb
 
 from django.shortcuts import redirect
 from django.utils import timezone
@@ -6,29 +7,29 @@ from django.utils import timezone
 from .models import *
 
 
-def odaDurumuDegistir():
-    guncel_zaman = timezone.now()
+# def odaDurumuDegistir():
+#     guncel_zaman = timezone.now()
 
-    checkouts = KonukCheckInveCheckOut.objects.filter(checkOut__lt=guncel_zaman)
-    checkins = KonukCheckInveCheckOut.objects.filter(checkIn__gte=guncel_zaman)
+#     checkouts = KonukCheckInveCheckOut.objects.filter(checkOut__lt=guncel_zaman)
+#     checkins = KonukCheckInveCheckOut.objects.filter(checkIn__gte=guncel_zaman)
 
-    if checkins:
-        for odaGiris in checkins:
-            oda = odaGiris.oda
-            oda.odaRezerveMi = True
-            oda.save()
-            print(f"Oda {oda.odaNumarasi}'nın Blokajlanmıştır!")
+#     if checkins:
+#         for odaGiris in checkins:
+#             oda = odaGiris.oda
+#             oda.odaRezerveMi = True
+#             oda.save()
+#             print(f"Oda {oda.odaNumarasi}'nın Blokajlanmıştır!")
 
-    if checkouts:
-        for cikisOdadurumuDegisti in checkouts:
-            oda = cikisOdadurumuDegisti.oda
-            oda.odaBosMu = True
-            oda.save()
-            print(f"Oda {oda.odaNumarasi} artık boştur!")
-            cikisOdadurumuDegisti.delete()
+#     if checkouts:
+#         for cikisOdadurumuDegisti in checkouts:
+#             oda = cikisOdadurumuDegisti.oda
+#             oda.odaBosMu = True
+#             oda.save()
+#             print(f"Oda {oda.odaNumarasi} artık boştur!")
+#             cikisOdadurumuDegisti.delete()
 
-    print("CheckIn Kontrol Modülü Devrede!")
-    print("CheckOut Kontrol Modülü Devrede!")
+#     print("CheckIn Kontrol Modülü Devrede!")
+#     print("CheckOut Kontrol Modülü Devrede!")
 
 
 # Mücahit Öğretti Luuwa "type hint"
@@ -52,32 +53,3 @@ def odastatus(kisi: KonukCheckInveCheckOut, odaID: int) -> None:
         kisi.oda.odaRezerveMi = True
         kisi.oda.save()
         print("Oda Rezerve Olmuştur")
-
-
-# print("Kişi Çekin: ",kisi.checkIn)
-
-# datetime_object = datetime.strptime(kisi.checkIn, '%Y-%m-%dT%H:%M')
-# print("datetime: ", datetime_object)
-# tarih = date(datetime_object.year,datetime_object.month,datetime_object.day)
-# print("Tarih Değişkeni: ", tarih)
-
-# if tarih == datetime.today():
-#     oda.odaBosMu = False
-#     oda.save()
-#     print("Oda Doludur!")
-# elif tarih > datetime.today():
-#     oda.odaRezerveMi = True
-#     oda.save()
-#     print("Oda Rezerve Olmuştur!")
-
-# print("Nasıl geliyor: ", kisi.checkIn)
-# print("Güncel Zaman:", guncel_zaman)
-
-# if kisi.checkIn <= guncel_zaman:
-#     oda.odaBosMu = False
-#     oda.save()
-#     print("Oda Durumu Değişmiştir!")
-# else:
-#     oda.odaRezerveMi = True
-#     oda.save()
-#     print("Oda Rezerve Olmuştur!")
